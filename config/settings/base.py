af3d6c27e937e24ebcfc8123070e2a5be7b24ef3
Mohammad Rezazade <mrprocs@gmail.com>
2025-08-29 05:49:12 +0200

    feat: update account hash and import account configuration in base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -121,4 +121,5 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+from accounts.config import *  # noqa: F403, E402
 from config.settings.celery import *  # noqa: F403, E402
